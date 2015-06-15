51300344a356ce57301469da5924a96b9d9ccb77
PASCO-BREILLOT Mathieu <mathieu.pasco@imie-rennes.fr>
2015-06-15 19:56:49 +0200

    Initial commit. Validation on textbox done. Design done.

---
+++ HangmanGame/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace HangmanGame
+{
+    partial class HangManGamePlayedForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxHangman = new System.Windows.Forms.PictureBox();
+            this.textBoxWordToFind = new System.Windows.Forms.TextBox();
+            this.buttonValidationWord = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHangman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxHangman, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxWordToFind, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonValidationWord, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(480, 495);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBoxHangman
+            // 
+            this.pictureBoxHangman.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBoxHangman, 3);
+            this.pictureBoxHangman.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxHangman.Location = new System.Drawing.Point(3, 3);
+            this.pictureBoxHangman.Name = "pictureBoxHangman";
+            this.pictureBoxHangman.Size = new System.Drawing.Size(474, 450);
+            this.pictureBoxHangman.TabIndex = 0;
+            this.pictureBoxHangman.TabStop = false;
+            // 
+            // textBoxWordToFind
+            // 
+            this.textBoxWordToFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxWordToFind.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxWordToFind, 2);
+            this.textBoxWordToFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBoxWordToFind.Location = new System.Drawing.Point(3, 462);
+            this.textBoxWordToFind.MaxLength = 47;
+            this.textBoxWordToFind.Name = "textBoxWordToFind";
+            this.textBoxWordToFind.Size = new System.Drawing.Size(374, 26);
+            this.textBoxWordToFind.TabIndex = 1;
+            this.textBoxWordToFind.TextChanged += new System.EventHandler(this.textBoxWordToFind_TextChanged);
+            // 
+            // buttonValidationWord
+            // 
+            this.buttonValidationWord.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonValidationWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonValidationWord.Location = new System.Drawing.Point(383, 459);
+            this.buttonValidationWord.Name = "buttonValidationWord";
+            this.buttonValidationWord.Size = new System.Drawing.Size(94, 33);
+            this.buttonValidationWord.TabIndex = 2;
+            this.buttonValidationWord.Text = "Valider";
+            this.buttonValidationWord.UseVisualStyleBackColor = true;
+            this.buttonValidationWord.Click += new System.EventHandler(this.buttonValidationWord_Click);
+            // 
+            // HangManGamePlayedForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 495);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "HangManGamePlayedForm";
+            this.Text = "HangMan";
+            this.TransparencyKey = System.Drawing.Color.DarkRed;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHangman)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBoxHangman;
+        private System.Windows.Forms.TextBox textBoxWordToFind;
+        private System.Windows.Forms.Button buttonValidationWord;
+    }
+}
+
---
+++ HangmanGame/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    public partial class HangManGamePlayedForm : Form
+    {
+        // Global Variables
+        private List<char> Letters = new List<char>
+        { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-'};
+        public HangManGamePlayedForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonValidationWord_Click(object sender, EventArgs e)
+        {
+            // To be implemented, the word must be checked regarding the word to find.
+        }
+
+        private void textBoxWordToFind_TextChanged(object sender, EventArgs e)
+        {
+            // Shortcut to textbox length
+            int tbLength = textBoxWordToFind.Text.Length;
+            // Check if the textbox is not empty
+            if (tbLength != 0)
+            {
+                // Check if the last caracter is one the allowed
+                if (!Letters.Contains(textBoxWordToFind.Text.Last()))
+                {
+                    string enumAllow = "";
+                    Letters.ForEach(delegate (char citem)
+                    {
+                        enumAllow += citem.ToString() + " | ";
+                    });
+                    textBoxWordToFind.Text = textBoxWordToFind.Text.Remove(tbLength - 1, 1);
+                    textBoxWordToFind.Select(tbLength, 0);
+                    MessageBox.Show("Le dernier caractère entré n'est pas valide. Il doit être un de ceux là : " + enumAllow, "Erreur de caractère", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                }
+            }
+        }
+    }
+}
