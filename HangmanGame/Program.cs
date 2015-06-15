c4a4f9c9e99a30894399ff59009c64dc936ed7ea
PASCO-BREILLOT Mathieu <mathieu.pasco@imie-rennes.fr>
2015-06-15 21:19:51 +0200

    Two more forms added to make a menu. The program is starting with menu. References to other form done in each constructor. Pass by an object then cast as it should. Buttons validation with enter added.

--- HangmanGame/Program.cs
+++ HangmanGame/Program.cs
@@ -16,7 +16,7 @@ namespace HangmanGame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HangManGamePlayedForm());
+            Application.Run(new ChooseGameMode());
         }
     }
 }
---
+++ HangmanGame/VersusMode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    public partial class VersusMode : Form
+    {
+        // Global Variables
+        ChooseGameMode formCGM;
+        public VersusMode(ChooseGameMode cgm)
+        {
+            InitializeComponent();
+            formCGM = cgm;
+        }
+
+        private void buttonValidateWordToFind_Click(object sender, EventArgs e)
+        {
+            SoloMode sm = new SoloMode(maskedTextBoxWordToFind.Text);
+            sm.Show();
+            formCGM.Close();
+
+        }
+
+        // Perform a click when button Enter is press on the keyboard
+        private void maskedTextBoxWordToFind_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                buttonValidateWordToFind.PerformClick();
+            }
+        }
+    }
+}
